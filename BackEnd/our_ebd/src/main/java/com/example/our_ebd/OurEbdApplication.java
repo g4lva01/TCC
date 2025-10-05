61f13d421db23fbbcec8614d8f2fa8e422483f64
g4lva01 <gabrielgalvao010@gmail.com>
2025-10-05 13:11:52 -0300

    Inicio do Back-End

---
+++ BackEnd/our_ebd/src/main/java/com/example/our_ebd/OurEbdApplication.java
@@ -0,0 +1,13 @@
+package com.example.our_ebd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurEbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurEbdApplication.class, args);
+	}
+
+}
---
+++ BackEnd/our_ebd/src/test/java/com/example/our_ebd/OurEbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.our_ebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurEbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
