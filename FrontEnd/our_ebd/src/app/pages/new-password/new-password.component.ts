e2f059af890f52018145a5fee42aaa17ab7b0c16
g4lva01 <gabrielgalvao010@gmail.com>
2025-10-27 15:22:22 -0300

    Configuração para salva nova senha no BD

--- FrontEnd/our_ebd/src/app/pages/new-password/new-password.component.ts
+++ FrontEnd/our_ebd/src/app/pages/new-password/new-password.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from "@angular/common";
-import { Router} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -12,31 +13,40 @@ import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors, 
 export class NewPasswordComponent {
   formulario = new FormGroup({
     nomeMat: new FormControl('', Validators.required),
-    nvSenha: new FormControl('', Validators.required),
+    nvSenha: new FormControl('', [Validators.required, this.senhaForteValidator]),
     cfNvSenha: new FormControl('', Validators.required)
   }, { validators: this.senhasIguaisValidator });
 
-  constructor(private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   alterarLogin() {
-    if (this.formulario.valid) {
-      console.log('Formulário enviado:', this.formulario.value);
-      this.router.navigate(['']); // redireciona para a tela principal
-    } else {
-      this.formulario.markAllAsTouched();
-    }
+    if (this.formulario.invalid) return;
+
+    const body = {
+      identificador: this.formulario.value.nomeMat,
+      novaSenha: this.formulario.value.nvSenha,
+      confirmarSenha: this.formulario.value.cfNvSenha
+    };
+
+    this.http.put('http://localhost:8080/api/login/alterar', body)
+      .subscribe({
+        next: res => {
+          alert('Senha alterada com sucesso!');
+          this.router.navigate(['/']);
+        },
+        error: err => alert('Erro ao alterar senha: ' + err.error)
+      });
   }
 
   senhasIguaisValidator(control: AbstractControl): ValidationErrors | null {
-    const senha = control.get('senha')?.value;
-    const cfSenha = control.get('cfSenha')?.value;
+    const senha = control.get('nvSenha')?.value;
+    const cfSenha = control.get('cfNvSenha')?.value;
     return senha === cfSenha ? null : { senhasDiferentes: true };
   }
-}
 
-function senhaForteValidator(control: AbstractControl): ValidationErrors | null {
-  const senha = control.value;
-  const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-  return regex.test(senha) ? null : { senhaFraca: true };
+  senhaForteValidator(control: AbstractControl): ValidationErrors | null {
+    const senha = control.value;
+    const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(senha) ? null : { senhaFraca: true };
+  }
 }
