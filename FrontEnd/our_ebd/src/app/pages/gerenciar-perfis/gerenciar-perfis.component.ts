4dcd03d3807efd0ed2b51bcdb50a42a41542c145
g4lva01 <gabrielgalvao010@gmail.com>
2025-11-10 23:29:10 -0300

    Ajuster na tela de gerenciamento de usuários

--- FrontEnd/our_ebd/src/app/pages/gerenciar-perfis/gerenciar-perfis.component.ts
+++ FrontEnd/our_ebd/src/app/pages/gerenciar-perfis/gerenciar-perfis.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../components/menu/menu.component';
 
@@ -11,14 +11,23 @@ import { MenuComponent } from '../../components/menu/menu.component';
 })
 export class GerenciarPerfisComponent {
   usuarios: any[] = [];
-  perfisDisponiveis = ['ALUNO', 'PROFESSOR', 'GESTOR'];
+  perfisDisponiveis = ['PROFESSOR', 'GESTOR'];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get<any[]>('/api/login/logins').subscribe(res => {
-      this.usuarios = res;
-    });
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const token = localStorage.getItem('token');
+
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+      
+      this.http.get<any[]>('http://localhost:8080/api/login/logins', { headers }).subscribe({
+        next: res => this.usuarios = res,
+        error: err => console.error('Erro ao buscar logins:', err)
+      });
+    }
   }
 
   togglePerfil(usuario: any, perfil: string) {
