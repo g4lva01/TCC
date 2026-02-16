13c3d679cb13c39204521dc01c8c9be61cd3577e
g4lva01 <gabrielgalvao010@gmail.com>
2026-02-16 18:38:39 -0300

    Ajuste no historico de frequencia

--- FrontEnd/our_ebd/src/app/pages/class-history/class-history.component.ts
+++ FrontEnd/our_ebd/src/app/pages/class-history/class-history.component.ts
@@ -4,20 +4,23 @@ import { MenuComponent } from '../../components/menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { ChamadaService } from '../../services/chamada.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-class-history',
-  imports: [CommonModule, MenuComponent, RouterLink],
+  imports: [CommonModule, MenuComponent, RouterLink, FormsModule],
   standalone: true,
   templateUrl: './class-history.component.html',
   styleUrls: ['./class-history.component.css']
 })
 export class ClassHistoryComponent implements OnInit {
   turma: string = '';
-
+  trimestreSelecionado: number = 1;
+  anoSelecionado: number = new Date().getFullYear();
+  anosDisponiveis: number[] = [];
   historico: {
     data: string;
-    presente: number;
+    presentes: number;
     levouBiblia: number;
     revistas: number;
   }[] = [];
@@ -30,11 +33,18 @@ export class ClassHistoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.turma = this.route.snapshot.paramMap.get('turma') || '';
+    
+    const anoAtual = new Date().getFullYear();
+    this.anosDisponiveis = Array.from({length: 5}, (_, i) => anoAtual - i);
+    
+    this.carregarHistorico();
+  }
 
+  carregarHistorico() {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
 
-    this.http.get<any[]>(`http://localhost:8080/api/presencas/historico/turma/${this.turma}`, { headers })
+    this.http.get<any[]>(`http://localhost:8080/api/presencas/historico/turma/${this.turma}?trimestre=${this.trimestreSelecionado}&ano=${this.anoSelecionado}`, { headers })
       .subscribe({
         next: res => this.historico = res,
         error: err => console.error('Erro ao buscar histórico:', err)
