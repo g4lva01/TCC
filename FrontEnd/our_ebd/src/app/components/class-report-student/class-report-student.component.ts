fc605be346e4d7be65c0184eb71c445da5ed353e
g4lva01 <gabrielgalvao010@gmail.com>
2026-03-10 22:15:24 -0300

    Ajuste para mostra somente o domingo do trimestre selecionado

--- FrontEnd/our_ebd/src/app/components/class-report-student/class-report-student.component.ts
+++ FrontEnd/our_ebd/src/app/components/class-report-student/class-report-student.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,25 +15,8 @@ export class ClassReportStudentComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    let alunoId: string | null = null;
-
-    if (typeof window !== 'undefined') {
-      alunoId = localStorage.getItem('alunoId');
-    }
-
-    if (!alunoId) {
-      console.warn('ID do aluno não encontrado!');
-      return;
-    }
-
-    this.http.get<any[]>(`/api/alunos/${alunoId}/relatorio`)
-      .subscribe({
-        next: data => this.relatorio = data,
-        error: err => console.error('Erro ao buscar relatório:', err)
-      });
-
     const anoAtual = new Date().getFullYear();
-    const anos = [anoAtual, anoAtual -1];
+    const anos = [anoAtual, anoAtual - 1];
 
     this.trimestres = anos.flatMap(ano => [
       `${ano}-T1`,
@@ -41,5 +24,24 @@ export class ClassReportStudentComponent implements OnInit {
       `${ano}-T3`,
       `${ano}-T4`
     ]);
+  }
+
+
+  onTrimestreChange(event: any) {
+    const valor = event.target.value;
+    if(!valor) return;
+
+    const [ano, trimestreStr] = valor.split('-T');
+    const trimestre = parseInt(trimestreStr, 10);
+
+    const alunoNome = localStorage.getItem('alunoNome');
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}`});
+
+    this.http.get<any[]>(`http://localhost:8080/api/presencas/relatorio/aluno/${alunoNome}/${ano}/${trimestre}`, {headers})
+      .subscribe({
+        next: data => this.relatorio = data,
+        error: err => console.error('Erro ao buscar relatório:', err)
+      });
   }
 }
