5fbfcb84ef37bdbba97d7fa8645a5a5680149642
g4lva01 <gabrielgalvao010@gmail.com>
2026-03-22 11:06:21 -0300

    Ajuste no script de criação do admin

--- Banco_de_Dados/criando_admin.sql
+++ Banco_de_Dados/criando_admin.sql
@@ -1,5 +1,9 @@
 INSERT INTO Perfil (nome)
-VALUES ('admin');
+VALUES ('aluno');
+INSERT INTO Perfil (nome)
+VALUES ('professor');
+INSERT INTO Perfil (nome)
+VALUES ('gestor');
 
 INSERT INTO Pessoa (nome,matricula)
 VALUES ('admin',999999);
@@ -7,5 +11,16 @@ VALUES ('admin',999999);
 INSERT INTO Usuario_Autenticacao (pessoa_id, senha, data_ultimo_login)
 VALUES (1, SHA2('7654321', 256), NOW());
 
-INSERT INTO Pessoa_Perfil (pessoa_id, perfil_id)
-VALUES (1, 1);
+INSERT INTO pessoa_perfil (pessoa_id, perfil_id)
+VALUES (1, 1); -- aluno
+INSERT INTO pessoa_perfil (pessoa_id, perfil_id)
+VALUES (1, 2); -- professor
+INSERT INTO pessoa_perfil (pessoa_id, perfil_id)
+VALUES (1, 3); -- gestor
+
+INSERT INTO usuario_autenticacao_roles (usuario_autenticacao_pessoa_id, roles)
+VALUES (1, 'ROLE_ALUNO');
+INSERT INTO usuario_autenticacao_roles (usuario_autenticacao_pessoa_id, roles)
+VALUES (1, 'ROLE_PROFESSOR');
+INSERT INTO usuario_autenticacao_roles (usuario_autenticacao_pessoa_id, roles)
+VALUES (1, 'ROLE_GESTOR');
