863b0a04ca8dcec892ac4efe72108676ccae2284
g4lva01 <gabrielgalvao010@gmail.com>
2026-04-23 21:42:25 -0300

    Configuração do leitor de QR-Code para preenche os campos matricula ou pesquisa

--- FrontEnd/our_ebd/src/app/pages/enroll/enroll.component.ts
+++ FrontEnd/our_ebd/src/app/pages/enroll/enroll.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { BarcodeFormat } from '@zxing/library';
+import e from 'express';
+import { log } from 'console';
 
 @Component({
   selector: 'app-enroll',
@@ -12,7 +14,7 @@ import { BarcodeFormat } from '@zxing/library';
   templateUrl: './enroll.component.html',
   styleUrl: './enroll.component.css'
 })
-export class EnrollComponent {
+export class EnrollComponent implements OnDestroy {
   novoAluno = {
     nome: '',
     dtNascimento: '',
@@ -25,6 +27,9 @@ export class EnrollComponent {
   campoSelecionado: string = '';
   formats: BarcodeFormat[] = [BarcodeFormat.QR_CODE];
   mostrarScanner: boolean = false;
+  availableDevices: MediaDeviceInfo[] = [];
+  selectedDevice: MediaDeviceInfo | undefined;
+  renderizarScanner: boolean = false;
 
 
   constructor(private http: HttpClient) {}
@@ -76,10 +81,50 @@ export class EnrollComponent {
   }
 
   onScanSuccess(valor: string) {
-    if (this.campoSelecionado) {
+    console.log('Valor lido:', valor);
+
+    if (this.campoSelecionado === 'busca') {
+      this.busca = valor;
+    } else if (this.campoSelecionado === 'matricula') {
       (this.novoAluno as any)[this.campoSelecionado] = valor;
     } else {
       this.novoAluno.matricula = valor;
     }
+
+    this.toggleScanner();
+  }
+
+  async toggleScanner() {
+    if(this.mostrarScanner) {
+      this.mostrarScanner = false;
+
+      this.renderizarScanner = false;
+      this.selectedDevice = undefined;
+    } else {
+      this.selectedDevice = undefined;
+      this.renderizarScanner = true;
+
+      setTimeout(() => {
+        this.mostrarScanner = true;
+      }, 200);
+    }
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]) {
+    this.availableDevices = devices;
+    if (devices && devices.length > 0) {
+      const backCamera = devices.find(d => d.label.toLowerCase().includes('back'));
+      this.selectedDevice = backCamera || devices[0];
+    }
+  }
+
+  onScanError(err: any) {
+    console.error('Erro no scanner:', err);
+    alert('Não foi possível acessar a câmera. Verifique permissão ou se o dispositivo está disponível.');
+  }
+
+  ngOnDestroy() {
+    this.mostrarScanner = false;
+    this.selectedDevice = undefined;
   }
 }
