71366996a44a8932700db714ef511e2847c46b04
g4lva01 <gabrielgalvao010@gmail.com>
2026-06-01 23:13:05 -0300

    Ajuste na permissão

--- BackEnd/our_ebd/src/main/java/com/example/our_ebd/config/CorsConfig.java
+++ BackEnd/our_ebd/src/main/java/com/example/our_ebd/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:4200")
+                        .allowedOriginPatterns("http://localhost:4200", "https://our-ebd.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
